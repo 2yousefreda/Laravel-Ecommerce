c9ccf0f90197c95393c0d12723021c7b02ffe4e1
2yousefreda <youesfreda456@gmail.com>
2025-04-03 02:46:34 +0200

    add product seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\product::factory(20)->create();
+        // \App\Models\product::factory(20)->create();
+        for ($i = 0; $i < 20; $i++) {
+            \App\Models\product::create([
+                'name'=>'pizza',
+                'price'=>10,
+                'quantity'=> 5,
+                'imagepath'=> 'products/32ln6YBtKwaVaswE6YEbms78877Y8dQ1xGSmPRVk.jpg',
+                'category_id'=> 4
+            ]);
+        }
         // User::factory(10)->create();
         // \App\Models\Admin::factory(3)->create();
         // User::factory()->create([
